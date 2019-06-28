081b241fa5094fc14033386dd0523ae443fe2806
Tech-Smarts <52301463+Tech-Smarts@users.noreply.github.com>
2019-06-27 19:02:05 -0500

    Update and rename Installer.sh to installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,7 @@ python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools --upgrade
 source env/bin/activate
 python -m pip install --upgrade https://github.com/googlesamples/assistant-sdk-python/releases/download/0.3.0/google_assistant_library-0.0.2-py2.py3-none-linux_armv7l.whl
-python -m pip install --upgrade https://github.com/googlesamples/assistant-sdk-python/releases/download/0.3.0/google_assistant_library-0.0.2-py2.py3-none-linux_armv7l.whl
+python -m pip install --upgrade google-auth-oauthlib[tool]
 # Authorize Google Assistant using Client Json File
 # Below, replace “THE NAME OF THE DOWNLOADED CLIENT JSON FILE” with the exact name of the client file you downloaded from google (Making sure that the client json file is in the Downloads folder)
 google-oauthlib-tool --client-secrets /home/pi/Downloads/THE NAME OF THE DOWNLOADED CLIENT JSON FILE --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
